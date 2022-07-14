8302fc686043abb8369c18da696f11d9952d3a37
GES233 <2580585287@qq.com>
2022-07-14 17:39:21 +0800

    feat(posts): 添加了帖子部分的数据表

---
+++ catcove/model/tables/contents/posts.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from sqlalchemy.orm import declarative_mixin, declared_attr
+
+from .. import *
+
+@declarative_mixin
+class Posts(Base):
+    __abstract__ = True
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+    @declared_attr
+    def owner(self):
+        return Column(
+            Integer,
+            ForeignKey("users.id"))
+    # status: `normal`, `hidden`, `deleted`.
+    status = Column(String(16), default="normal")
+    create_time = Column(DateTime, default=datetime.utcnow())
+    content = Column(Text)
+
+    @declared_attr
+    def __repr__(self) -> str:
+        return '<Post %s from %s at %s>' % (
+            self.id,
+            self.owner,
+            self.create_time
+        )
+
+
+class PostsUnderThread(Posts):
+    __tablename__ = "posts"
+
+    # parent = Column(Integer, ForeignKey("threads.id"))
+    loc = Column(
+        Integer,
+        comment="loc(location), i.e. index in thread."
+    )
+
+
+class UserPosts(Posts):
+    __tablename__ = "userposts"

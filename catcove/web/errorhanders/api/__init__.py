de9b1057dfe73c1941b613df4468c4c6606c38c3
GES233 <2580585287@qq.com>
2022-08-15 23:04:14 +0800

    添加了返回处理API的自定义错误

--- catcove/web/errorhanders/api/__init__.py
+++ catcove/web/errorhanders/api/__init__.py
@@ -1,13 +1,39 @@
 from sanic.errorpages import JSONRenderer
 from sanic.response import HTTPResponse
 
+from ....entities.schemas.api import OriginContentModel
+from ....services.render import render_api_resp
+from ...blueprints.api.helper.code import SERVER_ERROR
 
 class CustomJSONRenderer(JSONRenderer):
     def render(self) -> HTTPResponse:
         return super().render()
     
     def minimal(self) -> HTTPResponse:
-        return super().minimal(self)
+        output = self._generate_output(full=False)
+        return render_api_resp(
+            body_code=SERVER_ERROR,
+            body_info=output["message"],
+            body_org=OriginContentModel(
+                content_type="error",
+                data={
+                    "description": output["description"]
+                }
+            ),
+            status=self.status)
     
     def full(self) -> HTTPResponse:
-        return super().minimal(self)
+        output = self._generate_output(full=True)
+        return render_api_resp(
+            body_code=SERVER_ERROR,
+            body_info=output["message"],
+            body_org=OriginContentModel(
+                content_type="error detail",
+                data={
+                    "description": output["description"],
+                    "path": output["path"],
+                    "args": output["args"],
+                    "exceptions": output["exceptions"]
+                }
+            ),
+            status=self.status)

e85d3f0d636d117576871292f19bf43a108a7316
GES233 <2580585287@qq.com>
2022-09-24 14:19:29 +0800

    (TRY)[orm][refrac] 将 AsyncSession 变为 sessionmaker

--- catcove/usecase/manage.py
+++ catcove/usecase/manage.py
@@ -1,5 +1,6 @@
 from . import ServiceBase
-from sqlalchemy.ext.asyncio import AsyncSession
+# from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select, update, or_, table, values
 from bcrypt import gensalt, hashpw
 
@@ -13,7 +14,7 @@ from ..entities.tables.users import (
 class ManageService(ServiceBase):
     def __init__(
         self,
-        db_session: AsyncSession,
+        db_session: sessionmaker,
         user: Users,
         role: str = "",
         status: dict | None = None,
@@ -28,51 +29,53 @@ class ManageService(ServiceBase):
     async def get_role(self) -> bool:
         if not isinstance(self.user, Users):
             return False
-        async with self.db_session.begin():
-            smpt_for_spectator = select(Spectator).where(
-                Spectator.user_id == self.user.id
-            )
-            smpt_for_moderator = select(Moderator).where(
-                Moderator.user_id == self.user.id
-            )
-            _as_spectator = await self.db_session.execute(smpt_for_spectator)
-            _as_spectator = _as_spectator.scalars().first()
-            self.db_session.expunge(_as_spectator) if _as_spectator else ...
-            _as_moderator = await self.db_session.execute(smpt_for_moderator)
-            _as_moderator = _as_moderator.scalars().first()
-            self.db_session.expunge(_as_moderator) if _as_spectator else ...
+        async with self.db_session() as session:
+            async with session.begin():
+                stmt_for_spectator = select(Spectator).where(
+                    Spectator.user_id == self.user.id
+                )
+                stmt_for_moderator = select(Moderator).where(
+                    Moderator.user_id == self.user.id
+                )
+                _as_spectator = await session.execute(stmt_for_spectator)
+                _as_spectator = _as_spectator.scalars().first()
+                session.expunge(_as_spectator) if _as_spectator else ...
+                _as_moderator = await session.execute(stmt_for_moderator)
+                _as_moderator = _as_moderator.scalars().first()
+                session.expunge(_as_moderator) if _as_spectator else ...
 
         self.user_as_spectator = _as_spectator
         self.user_as_moderator = _as_moderator
 
         return True
 
-    async def be_spectator(self, password_: str) -> Spectator | None:
+    async def be_spectator(self, password: str) -> Spectator | None:
         if not isinstance(self.user, Users):
             return None
         """ use `Service.get_user` before. """
 
-        # Update role.
-        async with self.db_session.begin():
-            smpt = (
-                update(Users)
-                .where(Users.id == self.user.id)
-                .values({"role": "spactator"})
-            )
-            spactator = Spectator(user_id=self.user.id)
-            self.db_session.add(spactator)
-            await self.db_session.flush()
-            await self.db_session.execute(smpt)
+        _hashed_passwd = lambda x: hashpw(x.encode("utf-8"), gensalt(rounds=26))
+        hashed_passwd = _hashed_passwd(password)
+        spactator = Spectator(
+            user_id=self.user.id,
+            password=hashed_passwd,
+        )
 
-        # Insert data.
-        async with self.db_session.begin():
-            result = await self.db_session.execute(
-                select(Spectator).where(Spectator.user_id == self.user.id)
-            )
-            spactator = result.scalars().first()
-            spactator.encrypt_passwd(password_)
-            await self.db_session.flush()
-            self.db_session.expunge(spactator)
+        # Update role.
+        async with self.db_session() as session:
+            async with session.begin():
+                stmt = (
+                    update(Users)
+                    .where(Users.id == self.user.id)
+                    .values({"role": "spactator"})
+                )
+                
+                session.add(spactator)
+                await session.execute(stmt)
+                await session.flush()
+                session.expunge(spactator)
+        
+        return spactator
 
     async def be_moderator(self) -> Moderator | None:
         if not isinstance(self.user, Users):

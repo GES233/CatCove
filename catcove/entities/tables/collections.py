7a1135470972adf54d993c8055b3c7712f8be1b2
GES233 <2580585287@qq.com>
2022-09-28 12:06:48 +0800

    [tables][feat] 【尝试】添加了收藏的部分表（未并入 Base）

--- catcove/entities/tables/collections.py
+++ catcove/entities/tables/collections.py
@@ -1,19 +1,82 @@
-from sqlalchemy.orm import declarative_mixin
+from datetime import datetime
+from sqlalchemy.orm import declarative_mixin, declared_attr
 
 from . import *
 
 
 class CollectSets(Base):
     __tablename__ = "collect_sets"
+    id = Column(
+        Integer,
+        primary_key=True,
+        unique=True,
+        index=True,
+        comment="The ID of set.",
+    )
+    create_at = Column(
+        DateTime,
+        default=datetime.utcnow(),
+    )
+    name = Column(
+        String(64),
+        nullable=False,
+        index=True,
+        comment="The name of set.",
+    )
+    description = Column(String(1024))
+
+    owner_id = Column(ForeignKey("users.id"))
+    # owner = relationship()
+
+    # 属性
+    invisable = Column(
+        Boolean,
+        nullable=False,
+    )
+
+    # 和各个元素的关系
+    # 这部分就硬删除了，和核心业务没啥关系
     ...
+
+    def __repr__(self) -> str:
+        return "<CollectSets '{}' for user {}>".format(
+            self.name,
+            self.owner_id
+        )
 
 
 @declarative_mixin
-class CoolectItems(Base):
+class CollectItems(Base):
     __abstract__ = True
+    id = Column(
+        Integer,
+        primary_key=True,
+        unique=True,
+        index=True,
+    )
+    collect_time = Column(
+        DateTime,
+        default=datetime.utcnow(),
+    )
+    set_id = Column(ForeignKey("collect_sets.id"))
+
+    # `item_id` in sub-class.
+    """
+    @declared_attr
+    def belongs_to(self):
+        return relationship("CollectSets")"""
     ...
 
 
-# Thread(as index)
+class CollectedThread(CollectItems):
+    __tablename__ = ""
+
+    thread_id = Column(ForeignKey("thread.id"))
+    belongs_to = relationship("CollectSets")
+
+    def __repr__(self) -> str:
+        ...
+
+
 # UserPosts
 # Tag
